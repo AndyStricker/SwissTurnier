b3a4617d6d213f53ecc3307555340d606e017eb1
Andreas Stricker <andy@knitter.ch>
2020-01-05 15:39:27 +0100

    Add HTML ranking table

--- swissturnier/report.py
+++ swissturnier/report.py
@@ -98,6 +98,7 @@ class CheetahReport(Report):
 
 class HTMLPlayTable(CheetahReport):
     TITLE = 'Spielplan Badmintonturnier'
+
     def __init__(self, db):
         super(HTMLPlayTable, self).__init__(db, filename='playtable.tmpl')
 
@@ -118,54 +119,17 @@ class HTMLPlayTable(CheetahReport):
         }
 
 
-class RankingReport(Report):
+class HTMLRankingTableReport(CheetahReport):
     TITLE = 'Rangliste Badmintonturnier'
 
-    def __init__(self, db, path='reports/rankings/', filename='rankings.tmpl'):
-        self._db = db
-        self._path = path
-        self._filename = filename
+    def __init__(self, db):
+        super(HTMLRankingTableReport, self).__init__(db, filename='ranking.tmpl')
 
-    @property
-    def db(self):
-        return self._db
-
-    def create(self):
-        path = os.path.join(self._path, self._filename)
-        tmpl = file(path, 'r').read().decode('utf-8')
-        ns = self.get_namespace()
-        result = Cheetah.Template.Template(tmpl, searchList=[ns])
-        return result
-
-    def get_namespace(self):
-        categories = self._get_category_list()
-        rank_lists = []
-        for sex, category in categories:
-            ranks = self.db.store.find(
-                athrank.db.Athlete,
-                athrank.db.Athlete.total_points > 0,
-                sex=sex,
-                category=category
-            )
-            ranks.order_by(athrank.db.Athlete.rank, athrank.db.Athlete.number)
-            if ranks.count() > 0:
-                rank_lists.append((sex, category, ranks))
+    def get_namespace(self, session):
+        current_round = swissturnier.db.query_current_round(session)
+        ranks = session.query(Rankings).order_by('rank').all()
         return {
             'title': self.TITLE,
-            'categories': rank_lists,
+            'current_round': current_round,
+            'ranks': ranks,
         }
-
-    def _get_category_list(self):
-        db_categories = self.db.store.find(athrank.db.Category)
-        category_set = set()
-        for category in db_categories:
-            category_set.add((category.sex, category.category))
-        categories = list(category_set)
-        def category_cmp(a, b):
-            s = cmp(a[0], b[0])
-            if s != 0:
-                return s
-            else:
-                return -1 * cmp(int(a[1][1:]), int(b[1][1:]))
-        categories.sort(cmp=category_cmp)
-        return categories
