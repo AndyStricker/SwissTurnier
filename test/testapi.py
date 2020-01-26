53c2b01c6874b4f44e41c7e0ecccd4b82f2eef2b
Andreas Stricker <andy@knitter.ch>
2020-01-26 02:07:27 +0100

    Implement one more API unit test

--- test/testapi.py
+++ test/testapi.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 import swissturnier.api
 
 try:
@@ -18,7 +19,15 @@ class TestDB(unittest.TestCase):
         self.test_app = None
 
     @unittest.skipIf(paste is None, "Requires paste library")
-    def test_api(self):
-        req = self.test_app.get('/', headers={'Content-Type': 'application/json'})
-        self.assertEqual(req.status, 200)
+    def test_api_index(self):
+        r = self.test_app.get('/', headers={'Accept': 'text/html'})
+        self.assertEqual(r.status, 200)
+        r.mustcontain('<!DOCTYPE html>')
+        r.mustcontain('<title>Teams</title>')
 
+    @unittest.skipIf(paste is None, "Requires paste library")
+    def test_api_categories(self):
+        r = self.test_app.get('/v1/categories', headers={'Accept': 'application/json'})
+        self.assertEqual(r.status, 200)
+        data = json.JSONDecoder().decode(r.body.decode('UTF-8'))
+        self.assertIsInstance(data['count'], int)
