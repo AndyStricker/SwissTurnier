33b9abf2773ec02e76039d5769f7fc55595b4392
Andreas Stricker <andy@knitter.ch>
2020-01-29 00:01:20 +0100

    Add main application structure
    Using ES6 import statements

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+import * as _ from './underscore.js';
+import * as Backbone from './backbone.js';
+
+(function (_window, _document, _HTMLDocument) {
+	var main = {};
+
+	main.SwissTurnier = function () {
+		var me =  this;
+		me.context = 'Text';
+	};
+
+	main.SwissTurnier.prototype.get_context = function () {
+		var me = this;
+		return me.context;
+	};
+
+
+	HTMLDocument.prototype.ready = function () {
+		return new Promise(function(resolve, reject) {
+			if (_document.readyState === 'complete') {
+				resolve(_document);
+			} else {
+				_document.addEventListener('DOMContentLoaded', function() {
+				resolve(_document);
+			});
+						}
+		});
+	}
+	_document.ready().then(function (doc) {
+		//console.log('The anchor:', doc.getElementById('anchor1'));
+		doc.getElementById('anchor1').innerHTML = '<em>Hello World</em>';
+	});
+
+	return main;
+}(window, document, HTMLDocument));
